e0cd789d6273687eceebe492c9f014fe38d3578d
EgijaG <egija.graudina@gmail.com>
2022-12-08 21:01:43 +0200

    Boilerplate for UI and some small fixes

--- Main.py
+++ Main.py
@@ -3,20 +3,20 @@ from BlockCipherModes import *
 from DES_encryption import encrypt
 from DES_encryption import createRoundKeys
 
-Decryption = True
+decryption = True
 fileName = ''
 key = stringToBinary('testtest')
 content = readFileContent(fileName)
 binaryCypherText = []
-PlainText = []
+plainText = []
 initializationVector = ''
 
-if initializationVector == '' and not Decryption:
+if initializationVector == '' and not decryption:
     initializationVector = generateInitializationVector()
 
 content = checkIfBinary(content)
 
-roundKey = createRoundKeys(key,Decryption)
+roundKey = createRoundKeys(key,decryption)
 initalBinaryInitializationVector = stringToBinary(initializationVector)
 
 binaryInitializationVector = stringToBinary(initializationVector) #convert to initialization vector to binary
@@ -27,18 +27,18 @@ fullBinaryPlainText = addPaddingToBinaryPlainText(initalBinaryPlainText)
 binaryPlainTextArray = splitIn64Bits(fullBinaryPlainText) #splits it and returns array
 #initializationVector <-save this somewhere idk
 for binaryPlainText in binaryPlainTextArray:
-    if Decryption:
+    if decryption:
         binaryCBCText = encrypt(binaryPlainText,roundKey)
-        PlainText.append(xor(binaryCBCText,initalBinaryInitializationVector))
+        plainText.append(xor(binaryCBCText,initalBinaryInitializationVector))
         initalBinaryInitializationVector = binaryPlainText
     else:
         binaryCBCText = xor(binaryPlainText,initalBinaryInitializationVector)
         binaryCypherText.append(encrypt(binaryCBCText,roundKey))
         initalBinaryInitializationVector = binaryCBCText
 
-if Decryption:
+if decryption:
     writeContentToFile(binaryToString(removePaddingToBinaryPlainText
-                                      (arrayToString(PlainText))),
+                                      (arrayToString(plainText))),
                        fileName)
 else:
     writeContentToFile(arrayToString(binaryCypherText),fileName)
--- UserInterface.py
+++ UserInterface.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import filedialog
+import os
 window = tk.Tk()
 
 # Creating text for user information
@@ -9,7 +11,7 @@ greeting.pack()
 info.pack()
 
 # Setting window size
-window.geometry("800x600")
+window.geometry("1400x1000")
 
 # Choice of process - encryption or decryption
 choiceText = tk.Label(text="Would you like to ENCRYPT or DECRYPT your file?")
@@ -18,8 +20,32 @@ encryptBtn = tk.Button(text="Encrypt", bg='green')
 decryptionBtn = tk.Button(text="Decrypt", bg='purple')
 encryptBtn.pack()
 decryptionBtn.pack()
-# Creating buttons for different choices and method calling
 
+# User choosing .txt file from their system
+def browseFiles():
+    filename = filedialog.askopenfilename(title = "Select a File",
+                                          filetypes = [("Text files","*.txt*")])
+    os.startfile(os.path.abspath(filename))
+    fileLabel.configure(text="Opened file: " + filename)
+
+fileLabel = tk.Label(text="File selection")
+button_explore = tk.Button(
+                        text = "Browse Files",
+                        command = browseFiles)  
+button_exit = tk.Button(
+                     text = "Exit",
+                     command = exit)
+fileLabel.pack()
+button_explore.pack()
+button_exit.pack()
+
+# Creating buttons for different choices and method calling
+choiceText = tk.Label(text="Would you like to use AES or DES encryption standard?")
+choiceText.pack()
+aesBtn = tk.Button(text="AES", bg='blue')
+desBtn = tk.Button(text="DES", bg='grey')
+aesBtn.pack()
+desBtn.pack()
 # Implementing file browser
 
 # Implementing key input fields
