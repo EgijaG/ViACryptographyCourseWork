4c68c587126fbf8947d6acde08956587c46fe43c
markus-laskovskisbrangals <markus.laskovskisbrangals@va.lv>
2022-12-12 22:38:22 +0200

    Added conditional rendering rendering for window elements

--- UserInterface.py
+++ UserInterface.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 import os
+import AES
 window = tk.Tk()
 
 decryption = True
@@ -13,18 +14,29 @@ info = tk.Label(text="You should choose the process - ENCRYPT or DECRYPT and enc
 greeting.pack()
 info.pack()
 
+#Setting Windows title
+window.title('ViA Cryptography Course Work')
+
 # Setting window size
 window.geometry("1400x1000")
 
 # Change decription bool based on button clicked
 def shouldDecrypt(value):
     global decryption
+    print(value)
     decryption = value
+    fileLabel = tk.Label(text="File selection")
+    button_explore = tk.Button(
+                        text = "Browse Files",
+                        command = browseFiles)  
+    fileLabel.pack()
+    button_explore.pack()
+
 # Choice of process - encryption or decryption
 choiceText = tk.Label(text="Would you like to ENCRYPT or DECRYPT your file?")
 choiceText.pack()
-encryptBtn = tk.Button(text="Encrypt", bg='green' ,command=shouldDecrypt(False))
-decryptionBtn = tk.Button(text="Decrypt", bg='purple', command=shouldDecrypt(True))
+encryptBtn = tk.Button(text="Encrypt", bg='green' ,command= lambda: shouldDecrypt(False))
+decryptionBtn = tk.Button(text="Decrypt", bg='purple', command= lambda: shouldDecrypt(True))
 encryptBtn.pack()
 decryptionBtn.pack()
 
@@ -38,32 +50,32 @@ def browseFiles():
     fileName = os.path.abspath(fileName)
     fileLabel.configure(text="Opened file: " + fileName)
 
-fileLabel = tk.Label(text="File selection")
-button_explore = tk.Button(
-                        text = "Browse Files",
-                        command = browseFiles)  
+    # Creating buttons for different choices and method calling
+    choiceText = tk.Label(text="Would you like to use AES or DES encryption standard?")
+    choiceText.pack()
+    aesBtn = tk.Button(text="AES", bg='blue', command = lambda: run('AES.py'))
+    desBtn = tk.Button(text="DES", bg='grey', command = lambda: run('DES.py'))
+    aesBtn.pack()
+    desBtn.pack()
+
+fileLabel = tk.Label(text='')
+
 button_exit = tk.Button(
                      text = "Exit",
                      command = exit)
-fileLabel.pack()
-button_explore.pack()
 button_exit.pack()
+button_exit.place(x=1300, y=20)
 
 # Script running function
 def run(file):
     choiceText.configure(text="chosen file for running: "+ file)
     os.system(file)
-# Creating buttons for different choices and method calling
-choiceText = tk.Label(text="Would you like to use AES or DES encryption standard?")
-choiceText.pack()
-aesBtn = tk.Button(text="AES", bg='blue', command = run('AES.py'))
-desBtn = tk.Button(text="DES", bg='grey', command = run('DES.py'))
-aesBtn.pack()
-desBtn.pack()
 
-# Implementing key input fields
-initVector = tk.Entry(text="Please enter your initialization vector ")
-initVector.pack()
+    # Implementing key input fields
+    initVector = tk.Entry(text="Please enter your initialization vector ")
+    initVector.pack()
+
 
 
 window.mainloop()
+
