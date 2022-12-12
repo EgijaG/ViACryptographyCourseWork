44b108e24f94c9d8753984def1d480cd50968363
markus-laskovskisbrangals <markus.laskovskisbrangals@va.lv>
2022-12-12 20:59:02 +0200

    Added code from design branch

--- UserInterface.py
+++ UserInterface.py
@@ -1,5 +1,10 @@
 import tkinter as tk
+from tkinter import filedialog
+import os
 window = tk.Tk()
+
+decryption = True
+fileName = ''
 
 # Creating text for user information
 greeting = tk.Label(text="Hello, dear user. This is a program where you will be able to encrypt & decrypt your text files.")
@@ -9,23 +14,56 @@ greeting.pack()
 info.pack()
 
 # Setting window size
-window.geometry("800x600")
+window.geometry("1400x1000")
 
+# Change decription bool based on button clicked
+def shouldDecrypt(value):
+    global decryption
+    decryption = value
 # Choice of process - encryption or decryption
 choiceText = tk.Label(text="Would you like to ENCRYPT or DECRYPT your file?")
 choiceText.pack()
-encryptBtn = tk.Button(text="Encrypt", bg='green')
-decryptionBtn = tk.Button(text="Decrypt", bg='purple')
+encryptBtn = tk.Button(text="Encrypt", bg='green' ,command=shouldDecrypt(False))
+decryptionBtn = tk.Button(text="Decrypt", bg='purple', command=shouldDecrypt(True))
 encryptBtn.pack()
 decryptionBtn.pack()
-# Creating buttons for different choices and method calling
 
+# User choosing .txt file from their system
 # Implementing file browser
+def browseFiles():
+    global fileName
+    fileName = filedialog.askopenfilename(title = "Select a File",
+                                          filetypes = [("Text files","*.txt*")])
+    
+    fileName = os.path.abspath(fileName)
+    fileLabel.configure(text="Opened file: " + fileName)
+
+fileLabel = tk.Label(text="File selection")
+button_explore = tk.Button(
+                        text = "Browse Files",
+                        command = browseFiles)  
+button_exit = tk.Button(
+                     text = "Exit",
+                     command = exit)
+fileLabel.pack()
+button_explore.pack()
+button_exit.pack()
+
+# Script running function
+def run(file):
+    choiceText.configure(text="chosen file for running: "+ file)
+    os.system(file)
+# Creating buttons for different choices and method calling
+choiceText = tk.Label(text="Would you like to use AES or DES encryption standard?")
+choiceText.pack()
+aesBtn = tk.Button(text="AES", bg='blue', command = run('AES.py'))
+desBtn = tk.Button(text="DES", bg='grey', command = run('DES.py'))
+aesBtn.pack()
+desBtn.pack()
 
 # Implementing key input fields
-
-
-
+initVector = tk.Entry(text="Please enter your initialization vector ")
+initVector.pack()
 
 
 window.mainloop()
